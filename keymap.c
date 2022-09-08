2cfeccc27030f5d2f1bea116428ce0a5186f28d0
Mahadi-Ahmed <mahadi4@gmail.com>
2022-09-08 21:34:56 +0200

    better console log macro   after console press left to move into paranthesis

--- keymap.c
+++ keymap.c
@@ -110,8 +110,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case CSLOG:
             if (record->event.pressed) {
-                SEND_STRING("console.log()");
-            }
+                SEND_STRING("console.log()"SS_TAP(X_LEFT));
+            } 
             return false;
     }
     return true;
